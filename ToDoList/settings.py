71b875c435288ddb974ba5d4f0835d00f33ced27
Tushar Sethi <sethitushar2000@gmail.com>
2020-06-11 18:20:21 +0530

    Debug to false

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't)3*a&owqsu%=^i5i54jrf+n^t11dc2bxkxoo#mtb12wuv^55v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sethi-todoapp.herokuapp.com']
 
 
 # Application definition
