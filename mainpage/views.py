99201f66f962e1fb7f4bffac8d2ac838bb6b79f8
Tushar Sethi <sethitushar2000@gmail.com>
2020-06-12 11:26:31 +0530

    Another commit

--- mainpage/views.py
+++ mainpage/views.py
@@ -15,7 +15,9 @@ def main_screen(request):
         if user.last_name != '':
             user.first_name = user.first_name + ' ' + user.last_name
             user.last_name = ''
-            user.save()
+           
+        user.username = email
+        user.save()
 
         mylists = MyLists.objects.filter(email=user.email)
         list_nos = []
