dde2c686d411a905462d5317f34aaf8cd5c90cb9
Tushar Sethi <sethitushar2000@gmail.com>
2020-06-12 19:51:48 +0530

    Another commit

--- mainpage/adapters.py
+++ mainpage/adapters.py
@@ -1,6 +1,5 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.contrib.auth.models import User,auth
-from allauth.account.models import EmailAddress
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self,request,sociallogin):
@@ -11,4 +10,4 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
             if User.objects.filter(username=email).exists():
                 User.objects.filter(username=email).delete()
                 return super(SocialAccountAdapter,self).pre_social_login(request,sociallogin)
-            return super(SocialAccountAadapter,self).pre_social_login(request,sociallogin)
+            return super(SocialAccountAdapter,self).pre_social_login(request,sociallogin)
